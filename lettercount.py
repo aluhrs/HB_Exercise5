c770da8236918f2c696069680184c65f867111f9
Ashley Luhrs <aluhrs@gmail.com>
2014-02-15 18:31:24 -0800

    count the letters in a text

--- lettercount.py
+++ lettercount.py
@@ -1,21 +1,46 @@
+"""Write a program, lettercount.py, that opens a file named on the command line 
+and counts how many times each letter occurs in that file. 
+Your program should then print those counts to the screen, in alphabetical order.
+display:
+3
+1
+etc
+"""
+
+from sys import argv
+
+script, filename = argv
+
 # open the file and read it
-f = open("twain.txt")
-filetext = f.read(1)
+# then close it
+f = open(filename, "r")
+filetext = f.read()
+f.close()
 
-# loop through each letter
-# count each time a letter appears
-# print the counts on the screen
-# in alphabetical order
+# create an empty dictionary
 
-counter = 0
+d = {}
+
+# loop through each character
+# lowercase each character
+# determine whether the char is a letter
+# if the letter is in the dictionary
+# add 1 to the values
+# if the letter is not in the dictionary
+# add it and assign it a value of 1
 
 for char in filetext:
-	if ord(string.lower(char)) == ord(string.lower(char)):
-		
-		counter =+ 1
-	else:	
-		print 0
-print counter	
+	lowercase = char.lower()
+	if ord(lowercase) >= ord("a") and ord(lowercase) <= ord('z'):
+		if d.get(lowercase):
+			d[lowercase] += 1
+		if not d.get(lowercase): 
+			d[lowercase] = 1
+
+# print the keys(frequency of the letters appearing in the text) on the screen
+# in alphabetical order
+
+for key in sorted(d.keys()):
+	print "%d" % (d[key])
 
 
-f.close()
